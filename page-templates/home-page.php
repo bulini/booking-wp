87c9beb72bbc9f2429e2066ae1d63f1a9d6a8adf
Giuseppe Surace <pinobulini@gmail.com>
2015-12-09 01:15:39 +0100

    tabs

--- page-templates/home-page.php
+++ page-templates/home-page.php
@@ -40,83 +40,8 @@ global $options;
   </section>
   <!-- End About -->
 
-  <!-- Start Testimonials -->
-  <section class="testimonials">
-
-    <!-- Start Video Background -->
-    <div id="video_testimonials" data-vide-bg="video/ocean" data-vide-options="position: 0% 50%"></div>
-    <div class="video_gradient"></div>
-    <!-- End Video Background -->
-
-    <div class="container">
-      <div class="row">
-        <div class="preamble light col-md-12">
-          <h3>Testimonials</h3>
-        </div>
-
-        <div class="col-md-12">
-          <div id="owl-testimonials" class="owl-carousel owl-theme">
-
-            <!-- Start Container Item -->
-            <div class="item">
-              <div class="col-lg-12">
-                <blockquote class="quote">
-                  <cite>John Doe<span class="job">CEO - UOUapps</span></cite>
-                  <p class="stars">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                  </p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui.
-                </blockquote>
-              </div>
-            </div>
-            <!-- End Container Item -->
-
-            <!-- Start Container Item -->
-            <div class="item">
-              <div class="col-lg-12">
-                <blockquote class="quote">
-                  <cite>John Kowalski<span class="job">CEO - UOUapps</span></cite>
-                  <p class="stars">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                  </p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui.
-                </blockquote>
-              </div>
-            </div>
-            <!-- End Container Item -->
-
-            <!-- Start Container Item -->
-            <div class="item">
-              <div class="col-lg-12">
-                <blockquote class="quote">
-                  <cite>John Doe<span class="job">CEO - UOUapps</span></cite>
-                  <p class="stars">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                  </p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui.
-                </blockquote>
-              </div>
-            </div>
-            <!-- End Container Item -->
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- End Testimonials -->
+  <!-- testimonials -->
+  <?php get_template_part('template-parts/testimonials'); ?>
 
   <?php get_template_part('template-parts/tabs'); ?>
 
