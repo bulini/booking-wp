d827efcd32c9c954b09697d5dc65ee8dcfe6525b
bulini <pinobulini@gmail.com>
2015-12-29 17:04:20 +0100

    ipn almost ready

--- page-templates/ipn-empty.php
+++ page-templates/ipn-empty.php
@@ -21,15 +21,29 @@ $result = Ipn();
 if($result) {
   wp_reset_query();
   $booking = new WP_Query( array('post_type' => 'bookings','meta_key' => 'token', 'meta_value' => $result['item_number'],'posts_per_page' => 1));
-  while ($booking->have_posts()) : $booking->the_post();
+    while ($booking->have_posts()) : $booking->the_post();
+      $bid = get_the_id();
+      $status = 'booked';
+      $booking_data = array(
+        'ID'           => $bid,
+        'post_title'   => 'Instant Booking #'.$bid.' processata',
+        'post_content' => 'Operation time '.date("d/m/Y h:i:s").' changing status to '.$status,
+        'post_status' 	 =>	$status,
+      );
+        // Update the post into the database
+        wp_update_post( $booking_data );
+        // se non aggiorna... dio porco
+        update_post_meta($bid, 'payment_method', 'Paypal');
+        //update_post_meta($bid, 'owner_price', $owner_price);
+        //update_post_meta($bid, 'owner_message', $owner_message);
 
-    $message='reservation id = '. get_the_id().' code '.$result['item_number']. "\r\n";
-    $message.='payment status = '.$result['payment_status']. "\r\n";
-    $message.='details = '.$result['item_name']. "\r\n";
-    $subject = 'Conferma pagamento Paypal #'.$result['item_number'];
-    $headers = 'From: IPN Notification <info@mirkobeb.com>' . "\r\n";
-    mail('pinobulini@gmail.com', $subject, $message,$headers);
-  endwhile;
+        $message='reservation id = '. get_the_id().' code '.$result['item_number']. "\r\n";
+        $message.='payment status = '.$result['payment_status']. "\r\n";
+        $message.='details = '.$result['item_name']. "\r\n";
+        $subject ='Conferma pagamento Paypal #'$bid.'-'.$result['item_number'];
+        $headers ='From: IPN Notification <info@mirkobeb.com>' . "\r\n";
+        mail('pinobulini@gmail.com', $subject, $message,$headers);
+    endwhile;
 
 } else {
   die();
