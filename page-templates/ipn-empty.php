ff0584573dd89a3b7899a52746b234ae77eeffbd
bulini <pinobulini@gmail.com>
2015-12-29 16:55:21 +0100

    ipm improvements

--- page-templates/ipn-empty.php
+++ page-templates/ipn-empty.php
@@ -19,20 +19,19 @@ Template Name: Ipn response
 $result = Ipn();
 
 if($result) {
+  wp_reset_query();
+  $booking = new WP_Query( array('post_type' => 'bookings','meta_key' => 'token', 'meta_value' => $result['item_number'],'posts_per_page' => 1));
+  while ($booking->have_posts()) : $booking->the_post();
 
-  $booking = get_posts(array('post_type' => 'bookings','meta_key' => 'token', 'meta_value' => $result['item_number'],'posts_per_page' => 1));
-
-  $id_booking = $booking[0]->ID;
-
-  $message='reservation id = '. $id_booking.' code '.$result['item_number']. "\r\n";
-  $message.='payment status = '.$result['payment_status']. "\r\n";
-  $message.='details = '.$result['item_name']. "\r\n";
-  $subject = 'Conferma pagamento Paypal #'.$result['item_number'];
-  $headers = 'From: IPN Notification <info@mirkobeb.com>' . "\r\n";
-  mail('pinobulini@gmail.com', $subject, $message,$headers);
+    $message='reservation id = '. get_the_id().' code '.$result['item_number']. "\r\n";
+    $message.='payment status = '.$result['payment_status']. "\r\n";
+    $message.='details = '.$result['item_name']. "\r\n";
+    $subject = 'Conferma pagamento Paypal #'.$result['item_number'];
+    $headers = 'From: IPN Notification <info@mirkobeb.com>' . "\r\n";
+    mail('pinobulini@gmail.com', $subject, $message,$headers);
+  endwhile;
 
 } else {
-
   die();
 }
 ?>
